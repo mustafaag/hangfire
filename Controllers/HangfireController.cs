fe32dd897785494bc0c169b5e56d29a5d11f87dd
mustafaag <mustafaaga23@gmail.com>
2020-04-08 21:15:14 +0200

    first commit

---
+++ Controllers/HangfireController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hangfire_webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HangfireController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from hangfire Api");
+        }
+  
+    }
+}
